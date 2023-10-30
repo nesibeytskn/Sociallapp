b78d4481f56d0624f753a9e9433337439a273ebb
nesibeytskn <nesibeyetiskin1@gmail.com>
2023-10-30 23:34:08 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <Text>app</Text>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
